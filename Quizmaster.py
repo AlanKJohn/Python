80e18de59fa590408adcf6f921932baa85b7e9cc
PythonProgrammers8 <76901729+PythonProgrammers8@users.noreply.github.com>
2021-01-03 16:00:37 +0530

    Update and rename README.md to Quizmaster.py

---
+++ Quizmaster.py
@@ -0,0 +1,194 @@
+score=0
+print("Welcome to QUIZMASTER Super Quiz!!")
+print("your score is",score)
+A=input("Alright,, Ready for question number one? Let’s go! What has a head tail but has no body?")
+if A=="coin":
+	print("correct")
+	score=score+10
+	print("Your current score is",score)
+else:
+	print("incorrect")
+B=input("Which is the Largest Democracy in the World ?")
+if B=="India":
+	print("correct")
+	score=score+10
+	print("Your current score is",score)
+else:
+	print("incorrect")
+C=input("Which is the largest land animal in the world ?")
+if C=="elephant":
+	print("correct")
+	score=score+10
+	print("Your current score is",score)
+else:
+	print("incorrect")
+D=input("What is the middle name of Harry potter ?")
+if D=="James":
+	print("correct")
+	score=score+10
+	print("Your current score is",score)
+else:
+	print("incorrect")
+E=input("Which type of nuts do rich people like?")
+if E=="cashewnuts":
+	print("correct")
+	score=score+10
+	print("Your current score is",score)
+else:
+		print("incorrect")
+F=input("It belongs to you, but others uses it more, what is it ?")
+if F=="name":
+		print("correct")
+		score=score+10
+		print("your score is",score)
+else:
+		print("incorrect")
+						 		
+G=input("What should be broken before we use it ?")
+if G=="egg":
+	print("correct")
+	score=score+10
+	print("Your current score is",score)
+else:
+	print("incorrect")
+print("Now, are you ready for the LIGHTINING round which contains the toughest riddle? You get 20 points per question in this round.")
+print("Your score is",score)
+H=input("What can you catch, but can't throw it ?")
+if H=="cold":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+I=input(" I make a loud sound when I’m changing. When I do change, I get bigger but weigh less. What am I?")
+if I=="popcorn":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+J=input("They come out at night without being called, and are lost in the day without being stolen. What are they?")
+if J=="star":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+K=input("What is as big as an elephant, but weighs nothing at all?")
+if K=="elephant’s shadow":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+L=input("What has only two words, but has thousands of letters?")
+if L=="post office":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+print("Now you are entering the RISKY ROUND, in which you gain 20 points and lose 10 points for incorrect answer. Let's go")
+M=input("A container without hinges, lock or a key, yet a golden treasure lies inside me. What am I?")
+if M=="egg":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, Your score is",score)
+N=input("I can fly but I have no wings. I can cry but I have no eyes. Wherever I go darkness follows me. What am I?")
+if N=="cloud":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, Your score is",score)
+O=input("What can fill an entire room without taking up any space?")
+if O=="light":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, Your score is",score)
+P=input("What can you hold in your right hand but never in your left hand?")
+if P=="left hand":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, score is",score)
+Q=input("What comes once in a minute, twice in a moment, but never in a thousand years?")
+if Q=="the letter M":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, score is",score)
+R=input("What is harder to catch the faster you run?")
+if R=="breathe":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, score is",score)
+S=input("You will always find me in the past. I can be created in the present, But the future can never taint me. What am I?")
+if S=="history":
+	print("correct")
+	score=score+20
+	print("Your current score is",score)
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, score is",score)
+print("Now the unbeneficial round starts in this you will not gain points for correct answer, but will lose 10 points for incorrect answer, Let's get started")
+T=input("What is at the end of a rainbow?")
+if T=="the letter W":
+	print("correct")
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, score is",score)
+U=input("You can see me in water, but I never get wet. What am I?")
+if U=="reflection":
+	print("correct")
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, score is",score)
+V=input("What is easy to get into, but hard to get out of?")
+if V=="trouble":
+	print("correct")
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, score is",score)
+W=input("Two in a corner, 1 in a room, 0 in a house, but 1 in a shelter. What am I?")
+if W=="the letter R":
+	print("correct")
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, score is",score)
+X=input("What has cities, but no houses; forests, but no trees; and water, but no fish?")
+if X=="map":
+	print("correct")
+else:
+	print("incorrect")
+	score=score-10
+	print("Oh no wrong answer, score is",score)
+print("Founders: Alan, Malyadri")
+print("Quiz Developers: Ashwin,Nitin,Mohith")
+print("Code Developers: Malyadri, Alan")
+print("Debugger and Support: Nitin")
